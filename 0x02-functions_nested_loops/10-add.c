4750eadbc8f265f3f73b9d4d7c8b09a539cc2024
isaactanddoh <isaac.tanddoh@gmail.com>
2022-10-18 14:05:02 -0700

    description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - adds integers
- * nam1: integer 1
- * nam2: integer 2
+ * @nam1: integer 1
+ * @nam2: integer 2
  * Description: sums nam1 and nam2 (nam1 is a pupular name in ghana lol)
  * Return: sum
  */
