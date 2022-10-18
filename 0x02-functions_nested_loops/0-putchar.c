05a240f0d1671ecf5521ec9df28059476c262bbe
isaactanddoh <isaac.tanddoh@gmail.com>
2022-10-18 13:28:15 -0700

    puthar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,18 @@
 #include "main.h"
 
 /**
- * man - Entry Point
- * 
+ * main - Entry point
+ * Prints _putchar
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char word[5] = "_putchar";
+	char word[8] = "_putchar";
 	int i;
-	
-	for (i=0; i < 5; i++)
+
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 
